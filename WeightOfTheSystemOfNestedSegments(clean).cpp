e931fe1a48b5a15780c46ca348e51bf65e94f60c
jacob <jalbert90@gmail.com>
2022-11-14 20:05:46 -0500

    Remove high weights. Resort. Construct system. Solved.

--- WeightOfTheSystemOfNestedSegments(clean).cpp
+++ WeightOfTheSystemOfNestedSegments(clean).cpp
@@ -9,7 +9,7 @@ struct point {
 };
 
 void solve() {
-	int n, m, sum;
+	int n, m, sum = 0;
 	cin >> n >> m;
 
 	vector<point> points;
@@ -19,14 +19,22 @@ void solve() {
 		cin >> pt.coordinate >> pt.weight;
 		pt.id = i;
 		points.push_back(pt);
+		sum += pt.weight;
 	}
 
 	sort(points.begin(), points.end(), [](point first, point second) {return first.weight < second.weight;});
 
-	cout << endl;
+	for (int i = 0; i < m - 2 * n; i++) {
+		sum -= points[m - 1 - i].weight;
+		points.pop_back();
+	}
 
-	for (int i = 0; i < m; i++) {
-		cout << points[i].id << " " << points[i].coordinate << " " << points[i].weight << endl;
+	sort(points.begin(), points.end(), [](point first, point second) {return first.coordinate < second.coordinate;});
+
+	cout << sum << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << points[i].id << " " << points[2 * n - 1 - i].id << endl;
 	}
 }
 
